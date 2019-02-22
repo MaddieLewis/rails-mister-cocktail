67f2c1b3c92b8ae0a75b9fe972b9a27d0593f3df
Maddie Lewis <maddie@simplywellbeing.com>
2019-02-22 11:05:50 +0000

    Main structure complete

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,2 +1,31 @@
 class CocktailsController < ApplicationController
+  # before_action :set_cocktail, only: [:show, ]
+  @image_urls = %w[https://images.pexels.com/photos/53025/pexels-photo-53025.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260 https://images.pexels.com/photos/613037/pexels-photo-613037.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260 https://images.pexels.com/photos/1189260/pexels-photo-1189260.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260]
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+    @dose = Dose.new
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+
+Cocktail.create(name: "test 1")
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "Pina colada")
